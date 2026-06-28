fd311d5e359df334f960b24dc914270bfff994b3
subliminaled <ed.castaneda@gmail.com>
2026-06-27 23:26:34 -0700

    Adding Nate's video link

--- src/data/stories.ts
+++ src/data/stories.ts
@@ -40,7 +40,7 @@ const stories: Story[] = [
     person: 'Nate Fieldson',
     excerpt:
       'Strong, purposeful steel.',
-    youtubeId: '',
+    youtubeId: '45wolv_NDAfkVHix',
     thumbnail: '/images/thumb/knife.png',
   },
 ];
